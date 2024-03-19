c558e7b22e69486aede0f82dd1561367bb39300a
Thomas Hirter <15847494+Memurame@users.noreply.github.com>
2024-03-19 14:49:27 +0100

    add light gallery to testimonial preview (#81)

--- app/Views/testimonial/public/index.php
+++ app/Views/testimonial/public/index.php
@@ -30,6 +30,7 @@
     </div>
 
     <script src="https://kit.fontawesome.com/46246584f2.js" crossorigin="anonymous"></script>
+    <script src="<?= base_url() ?>dist/libs/fslightbox/index.js?1692870487" defer></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
